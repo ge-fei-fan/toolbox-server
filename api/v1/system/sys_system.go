4ef5cad8d87cbda2403a6b8be20070aeb21fc010
ge-fei-fan <754277710@qq.com>
2023-08-13 21:53:45 +0800

    test

--- api/v1/system/sys_system.go
+++ api/v1/system/sys_system.go
@@ -6,8 +6,6 @@ import (
 	"toolbox-server/config"
 	"toolbox-server/global"
 	"toolbox-server/model/common/response"
-	"toolbox-server/model/system"
-	"toolbox-server/utils/cron"
 )
 
 type SystemApi struct{}
@@ -35,18 +33,7 @@ func (s *SystemApi) Mode(c *gin.Context) {
 	response.OkWithData(data, c)
 }
 func (s *SystemApi) Test(c *gin.Context) {
-	_, err := global.TOOL_SCHEDULER.Cron("*/5 * * * *").Tag("测试").DoWithJobDetails(cron.AutoCollect)
-	if err != nil {
-		global.TOOL_LOG.Error("错误", zap.Error(err))
-	}
-	cc := system.SysCrontab{
-		Func:   "AutoCollect",
-		Cron:   "*/5 * * * *",
-		Tag:    "测试",
-		Status: 0,
-	}
-	global.TOOL_DB.Create(&cc)
-	response.Ok(c)
+	response.OkWithMessage("哈哈哈", c)
 }
 
 func (s *SystemApi) GetConfig(c *gin.Context) {
